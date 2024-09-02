1a2c9359a4b63cb02abfbf8bf3c7a005fb228ded
Thanvesh <nanicherry2312@gmail.com>
2024-09-02 14:02:11 +0530

    new6

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://bookingserver-1.onrender.com',
+  baseURL: 'https://bookingserver-1.onrender.com/api',
 });
 
 export default api;
